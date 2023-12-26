6ebedbc1bfb3d7516c5bdceebe849bea9057e073
Nathan Soussana <nathan.soussana@gmail.com>
2023-12-26 20:55:51 +0100

    Installed React Router and created basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.sass'
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About</h1>
+  )
+}
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
