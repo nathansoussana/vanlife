1822f7e846b427a94528255e550cbd433a77d5e9
Nathan Soussana <nathan.soussana@gmail.com>
2023-12-27 12:33:09 +0100

    Finalized home page component and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.sass'
-
-function Home() {
-  return (
-    <h1>Home</h1>
-  )
-}
-
-function About() {
-  return (
-    <h1>About</h1>
-  )
-}
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="global_container">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.sass'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.content_container}>
+      <Link to="/" className={styles.logo}>
+        #VANLIFE
+      </Link>
+      <div className={styles.navigation_menu}>
+          <Link to="/about" className={styles.menu_item}>
+            About
+          </Link>
+          <Link to="#" className={styles.menu_item}>
+            Vans
+          </Link>     
+      </div>
+    </nav> 
+  )
+}
